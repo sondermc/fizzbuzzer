81afdf0797747bf6be555d49a741fd372e4098c9
Chris Sondermeijer <chris.sondermeijer@gmail.com>
2024-02-01 21:23:54 +0100

    adding unittests for the first iterations

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,64 @@
 pub mod fizzbuzzer;
+
+#[cfg(test)]
+mod tests {
+    use fizzbuzzer;
+    use super::*;
+
+    #[test]
+    fn test_1_eq_one() {
+        let expected = "one";
+        let actual = fizzbuzzer::fizzbuzzer(1);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_2_eq_one() {
+        let expected = "two";
+        let actual = fizzbuzzer::fizzbuzzer(2);
+        assert_eq!(actual, expected);
+    }
+    
+    #[test]
+    fn test_modulo_3_eq_fizz() {
+        let expected = "fizz";
+        let actual = fizzbuzzer::fizzbuzzer(3);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_modulo_3_not_eq_three() {
+        let expected = "three";
+        let actual = fizzbuzzer::fizzbuzzer(3);
+        assert_ne!(actual, expected);
+    }
+
+    #[test]
+    fn test_modulo_5_eq_five() {
+        let expected = "buzz";
+        let actual = fizzbuzzer::fizzbuzzer(5);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_modulo_5_not_eq_five() {
+        let expected = "five";
+        let actual = fizzbuzzer::fizzbuzzer(5);
+        assert_ne!(actual, expected);
+    }
+
+    #[test]
+    fn test_modulo_15_eq_fifteen() {
+        let expected = "fizzbuzz";
+        let actual = fizzbuzzer::fizzbuzzer(15);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_modulo_15_not_eq_fifteen() {
+        let expected = "fifteen";
+        let actual = fizzbuzzer::fizzbuzzer(15);
+        assert_ne!(actual, expected);
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use fizzbuzz::fizzbuzzer::fizzbuzzer;
+
 fn main() {
-    for c in 1..100 {
+    for c in 1..101 {
         let answer = fizzbuzzer(c);
         println!("{} -> {}", c, answer);
     }
